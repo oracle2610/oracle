5660f0e02b123400ec3b3359243cb7931b6051ef
oracle2610 <111477385+oracle2610@users.noreply.github.com>
2022-08-23 23:17:25 +0800

    Add files via upload

---
+++ makefile.sh
@@ -0,0 +1,11 @@
+README.md:
+
+       touch README.md
+
+       echo "Title of the Project: __Peer-graded Assignment: Bash, Make, Git and Github__" >> README.md
+       echo "\nDate make ran at: " >> README.md
+
+       date >> README.md
+
+       echo "\nNumber of lines file guessinggame.sh contains:"  >> README.md
+       cat guessinggamr.sh | wc -l >> README.md
